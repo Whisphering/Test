1860b50ce078e4c38ae5beb5c5154654d4808ee9
KaoKsn <mayurakrishna.23@gmail.com>
2024-12-27 23:54:38 +0530

    UPdate hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@ void add(int,int);
 int sum(int, float); 
 int main()
 {
+//Commented.
 	printf("Hello, World\n"); // Added in this line .
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
