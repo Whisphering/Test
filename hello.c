9de2f47cd1d3046451ee9784a30c9c722da90200
KaoKsn <mayurakrishna.23@gmail.com>
2024-12-27 18:36:04 +0530

    Remove an empty line staying the the local repo

--- hello.c
+++ hello.c
@@ -5,6 +5,5 @@ int main()
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
 	printf("This line was added staying in the origin.\n");
-
 	printf("This line was added staying the remote repo, but this time we are pulling without only changes present in the remote\n");
 }
