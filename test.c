7f09110ba6b5e6aa38ca9cf36db6fc577cc550ae
KaoKsn <mayurakrishna.23@gmail.com>
2024-12-27 23:13:15 +0530

    Update test.c

--- test.c
+++ test.c
@@ -6,4 +6,5 @@ int main(void)
 		printf("This line was added after last action in the local");
 		printf("This line was added in the remote after the last action \n");
 		printf("Line added after resolving conflicts before pushing\n");
+		printf("\nThese were the contents present in the test.c file");
 }
