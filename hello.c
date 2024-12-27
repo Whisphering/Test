34464752160b505af86616ff91fa3a44a23d63ed
Grub <justlaptop.2306@gmail.com>
2024-12-27 23:44:00 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,7 +6,8 @@ int main()
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
 	//printf("Hello, buddy I was added only in the origin.");
-// We commented the above line and then added an extra line i.e this line.
+// We commented the above line and then added an extra line i.e this line. No bothering of conflicts when you have only changed the contents here.
+	// What if I change the contents in both the places.
 	printf("What!\n");
 	printf("Hello.txt");
 	printf("Just the way you wanted!\n");
