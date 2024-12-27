68ab3de39004a49af41ce615b61c513eb28e7758
Grub <justlaptop.2306@gmail.com>
2024-12-27 23:29:58 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,4 +4,5 @@ int main()
 	printf("Hello, World\n");
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
+	printf("Hello, buddy I was added only in the origin.");
 }
