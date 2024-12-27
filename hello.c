eb8803388a781d5e4887ed0f181c6ea825256858
Grub <justlaptop.2306@gmail.com>
2024-12-27 23:42:31 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,8 @@ int main()
 	printf("Hello, World\n");
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
-	printf("Hello, buddy I was added only in the origin.");
+	//printf("Hello, buddy I was added only in the origin.");
+// We commented the above line and then added an extra line i.e this line.
 	printf("What!\n");
 	printf("Hello.txt");
 	printf("Just the way you wanted!\n");
