f0e11bb276d93ad3bdc804ae9998e9a73f0a599a
KaoKsn <mayurakrishna.23@gmail.com>
2024-12-27 23:23:48 +0530

    Reset from older version to newer version

--- test.c
+++ test.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-		printf("test.c");
-		printf("This code was written on in the local before pulling");
-		printf("This line was added after last action in the local");
-		printf("This line was added in the remote after the last action \n");
-		printf("Line added after resolving conflicts before pushing\n");
+	printf("test.c");
+	printf("This code was written on in the local before pulling");
+	printf("This line was added after last action in the local");
 }
