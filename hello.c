02f34c293fc45d777cf30e1e30c12316b8dbaffe
KaoKsn <mayurakrishna.23@gmail.com>
2024-12-27 23:35:38 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,4 +5,5 @@ int main()
 	printf("This was line was added in the local repo\n");
 	printf("This line was added after resolving the conflicts that arose while pulling.\n");
 	printf("Hello, buddy I was added only in the origin.");
+	printf("What!\n");
 }
